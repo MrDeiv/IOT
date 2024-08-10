d8e2effecfac1c6289da2e0f253758e1c6d9e889
Nicola Deidda <nicola.deidda@icloud.com>
2024-08-10 16:31:54 +0200

    Update bot.py

--- birdbusterbot/bot.py
+++ birdbusterbot/bot.py
@@ -6,7 +6,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.id}')
 
 
-app = ApplicationBuilder().token("6710606204:AAF7zQglSskTxsq3ItcAdPZSZhNM2R260ys").build()
+app = ApplicationBuilder().token("TOKEN").build()
 
 app.add_handler(CommandHandler("start", hello))
 
